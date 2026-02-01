f083cb1dd162ec5d21d4dbde125abfcdee73115b
Chris <cristru8@gmail.com>
2026-02-01 08:40:53 -0500

    configure baseURLs for deployment

--- src/redux/api/axiosConfig.js
+++ src/redux/api/axiosConfig.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 // During debugging we default dev URL to local backend on port 8000
 const devUrl = import.meta.env.VITE_API_DEV_URL || 'http://localhost:8000';
-const prodUrl = import.meta.env.VITE_API_PROD_URL || 'https://document-collector-7p0a.onrender.com';
+const prodUrl = import.meta.env.VITE_API_PROD_URL || 'https://document-collector-ec288zdc1-chiss7s-projects.vercel.app';
 const mode = import.meta.env.MODE; // 'development' or 'production'
 
 const baseURL = import.meta.env.VITE_API_BASE_URL || (mode === 'production' ? prodUrl : devUrl);
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
+const devUrl = import.meta.env.VITE_API_AUTH_DEV_URL || 'http://localhost:8080/api';
+const prodUrl = import.meta.env.VITE_API_AUTH_PROD_URL || 'https://tu-backend-auth.com/api';
+const mode = import.meta.env.MODE; // 'development' or 'production'
+
+const baseURL = import.meta.env.VITE_API_BASE_URL || (mode === 'production' ? prodUrl : devUrl);
+console.log(import.meta.env.MODE);
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: baseURL,
 });
 
 api.interceptors.request.use((config) => {
