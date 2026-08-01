62281015557a2190fa07e884ac97de2bebe60779
Chris <cristru8@gmail.com>
2026-07-31 21:47:53 -0500

    feat(login): redesign login with editorial light theme and full-height layout

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <Router>  {/* Sin basename por ahora */}
-      <div className="font-sans">
+      <div className="font-sans flex flex-col min-h-screen">
         <Header />
         <Routes>
           {/* Rutas públicas */}
